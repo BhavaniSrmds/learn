37ba0ba0c8cfb55fc321d5b51bff39cdc2ae0b90
bhavani <bhavani@webileapps.com>
2022-04-26 16:43:51 +0530

    project

---
+++ src/TestPractise/testclass.java
@@ -0,0 +1,12 @@
+package TestPractise;
+
+public class testclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("print a");
+
+	}
+
+}
